83fa9100b95111a463a1c5b8c37fe8fd13b10095
Henrik Andersson <hean01@users.sourceforge.net>
2014-10-14 23:54:56 +0200

    Added plugin.register() and plugin.URI_PREFIX

--- src/js/plugin.c
+++ src/js/plugin.c
@@ -29,22 +29,48 @@ _js_plugin_search(js_State *state)
   js = js_touserdata(state, 0, "instance");
 
   g_log(DOMAIN, G_LOG_LEVEL_MESSAGE,
-	"[%s]: Register of search API function.", js->provider->id);
+	"[%s]: register handler function for search.", js->provider->id);
 
   /* store search function in the registry */
   js_setregistry(state, "plugin.search");
+}
+
+static void
+_js_plugin_register(js_State *state)
+{
+  const gchar *uri;
+  js_provider_t *js;
+  js = js_touserdata(state, 0, "instance");
+
+  uri = js_tostring(state, 1);
+
+  g_log(DOMAIN, G_LOG_LEVEL_MESSAGE,
+	"[%s]: register handler function for uri '%s'", js->provider->id, uri);
+
+  /* store uri in the registry */
+  js_setregistry(state, uri);
 }
 
 
 void
 js_plugin_init(js_State *state, js_provider_t *instance)
 {
+  gchar uri_prefix[512];
+  g_snprintf(uri_prefix, sizeof(uri_prefix), "/providers/%s", instance->provider->id);
+
   js_newobject(state);
   {
     js_getproperty(state, 0, "prototype");
     js_newuserdata(state, "instance", instance);
+
+    js_pushstring(state, uri_prefix);
+    js_defproperty(state, -2, "URI_PREFIX", JS_READONLY);
+
     js_newcfunction(state, _js_plugin_search, 0);
     js_defproperty(state, -2, "search", 0);
+    js_newcfunction(state, _js_plugin_register, 0);
+    js_defproperty(state, -2, "register", 0);
+
   }
 }
 
