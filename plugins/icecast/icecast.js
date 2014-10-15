c9a22652b1db39c1154da247154c32bc7f578f99
Henrik Andersson <hean01@users.sourceforge.net>
2014-10-16 00:13:53 +0200

    Make icecast register start page and genres.

--- plugins/icecast/icecast.js
+++ plugins/icecast/icecast.js
@@ -4,6 +4,41 @@
 	'base_uri': 'http://dir.xiph.org'
     };
 
+    var genres = [
+	"80s",
+	"Alternative",
+	"Ambient",
+	"Breaks",
+	"Chillout",
+	"Dance",
+	"Dubstep",
+	"Drumnbass",
+	"Electronic",
+	"Funk",
+	"Gothic",
+	"Harcore",
+	"Hardstyle",
+	"Hardtrance",
+	"Hip Hop",
+	"House",
+	"Jazz",
+	"Jungle",
+	"Lounge",
+	"Metal",
+	"Minimal",
+	"Mixed",
+	"Pop",
+	"Progressive",
+	"Punk",
+	"Radio",
+	"Rock",
+	"Techno",
+	"Top40",
+	"Trance",
+	"Tribal",
+	"Various"
+    ];
+
     function getValue(content, start, end) {
 	var s = content.indexOf(start);
 	if (s < 0) return null;
@@ -16,14 +51,15 @@
 	return content.slice(s);
     };
 
-    function scrape_page(result, doc, limit)
+    function scrape_page(doc, limit)
     {
 	var item = {};
+	var result = [];
 
 	item.type = "radiostation";
 	item.metadata = {};
 
-	while(1)
+	while(1 && limit != 0)
 	{
 	    var str = "<tr class=\"row";
 	    var s = doc.indexOf(str, 2);
@@ -37,19 +73,51 @@
 	    if (s > -1) str = getValue(str, "');\">", "</a>");
 	    item.metadata.title = str;
 
-
 	    // description [optional]
 	    str =  getValue(doc, "<p class=\"stream-description\">","</p>");
 	    if (str) item.metadata.description = str;
 
-	    service.log("Station title: " + JSON.stringify(item));
+	    result.push(item);
+	    limit = limit - 1;
 	}
+
+	return result;
     };
+
+    /* setup start page */
+    plugin.register("/", function(path, offset, limit) {
+	var res = [];
+	var item = {};
+
+	item.type = "folder";
+	item.metadata = {};
+
+	genres.forEach(function(entry) {
+	    item.uri = "/genre/" + entry;
+	    item.metadata.title = entry;
+	    res.push(item);
+	});
+
+	return res;
+    });
+
+    /* add handlers for each genre */
+    genres.forEach(function(entry) {
+	/* register each entry point */
+	plugin.register("/genre/" + entry, function(path, offset, limit) {
+
+	    res = http.get(constants.base_uri + "/by_genre/" + entry);
+	    if (res.status != 200) return [];
+
+	    return scrape_page(res.body, limit);
+	});
+    });
+
 
     plugin.search(function(result, keywords, limit) {
 	kw = keywords.join("+");
 	res = http.get(constants.base_uri + "?search=" + kw);
-	scrape_page(result, res.body, limit);
+	scrape_page(res.body, limit);
     });
 
 }) (this);
