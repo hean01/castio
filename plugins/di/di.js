d842afe501b9fe102db326a890b0b0a5f5fdce46
Henrik Andersson <hean01@users.sourceforge.net>
2014-10-16 21:36:39 +0200

    Initial page scrape of DI.fm

--- plugins/di/di.js
+++ plugins/di/di.js
@@ -1,9 +1,61 @@
 (function() {
+    var constants = {
+	'base_uri': 'http://www.di.fm'
+    };
 
-    plugin.search(function(results, keywords, limit) {
+    function getValue(content, start, end) {
+	var s = content.indexOf(start);
+	if (s < 0) return null;
+	s = s + start.length;
+	if (end != null) {
+	    var e = content.indexOf(end, s);
+	    if (e < 0) return null;
+	    return content.slice(s, e);
+	}
+	return content.slice(s);
+    };
 
-	service.log("Keywords: " + JSON.stringify(keywords));
 
+    function scrape_page(doc, limit) {
+	first = true;
+	result = [];
+
+	while(1) {
+
+	    var item = {};
+	    item.type = "radiostation";
+	    item.metadata = {};
+
+	    var str = "<li data-channel-id=\"";
+	    var s = doc.indexOf(str, 2);
+	    if (s < 0) break;
+	    doc = doc.slice(s);
+
+	    /* skip first occurence */
+	    if (first) {
+		first = false;
+		continue;
+	    }
+
+	    // get key and id
+	    str = getValue(doc, "data-channel-key=\"", "\"");
+	    if (str == null) continue;
+	    item.uri = "http://listen.di.fm/public3/"+str+".pls";
+
+	    // get station name
+	    str = getValue(doc, "<span>", "</span>");
+	    if (str == null) continue;
+	    item.metadata.title = str;
+
+	    result.push(item);
+	}
+	return result;
+    }
+
+    plugin.register("/", function(path, offest, limit) {
+	res = http.get(constants.base_uri);
+	if (res.status != 200) return [];
+	return scrape_page(res.body, limit);
     });
 
 }) (this);
