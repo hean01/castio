2f8c51ce3739576cf5253b8f977bbbfa91d5c361
Henrik Andersson <hean01@users.sourceforge.net>
2015-02-04 20:19:12 +0100

    Initial rewrite of plugin to use souncloud api

--- plugins/soundcloud/soundcloud.js
+++ plugins/soundcloud/soundcloud.js
@@ -1,85 +1,186 @@
 (function() {
 
-    var constants = {
-	'base_uri': 'https://api.soundcloud.com',
-	'client_id': 'b0479634e56c0feaa933fffa85c7b33b'
+    // SoundcloudClient javascript object
+    function SoundcloudClient() {
+	this.constants = {
+	    'api_uri': 'https://api.soundcloud.com',
+	    'client_id': 'b0479634e56c0feaa933fffa85c7b33b',
+	    'client_secret': '2e6dcc195af704f07daf6889a734ab96'
+	};
+
+	// private helper functions, prefixed with _
+	this._paramsToQuery = function(parameters) {
+	    var arr = [];
+
+	    if (parameters == null)
+		return "";
+
+	    for (var key in parameters) {
+		arr.push(key + "=" + parameters[key]);
+	    }
+
+	    if (arr.length() == 0)
+		return "";
+
+	    return "?" + arr.join("&");
+	}
+
+	this._pathForUser = function(user, path) {
+	    if (user == "me")
+		return "/me" + path;
+	    else
+		return "/users/" + user + path;
+	}
+
+	this._performRequest = function(path, headers, params) {
+	    params.client_id = this.constants.client_id;
+
+	    var url = this.constants.api_uri + path
+		+ this._paramsToQuery();;
+
+	    var response = http.get(url, headers);
+	    if (response.status != 200) {
+		service.warning("Failed to get resource, status "
+				+ response.status);
+		return null;
+	    }
+
+	    return JSON.parse(response.body);
+	}
+
+	// Get tracks from stream
+	this.getStream = function(user_id, offset, limit) {
+	    var params = {};
+	    var headers = {};
+
+	    params.offset = offset;
+	    params.limit = limit;
+
+	    headers.Accept = "application/json";
+
+	    return this._performRequest(_pathForUser(user_id, "/activities/tracks/affiliated"),
+					headers, params);
+	}
+
+	this.getCategories = function(offset, limit) {
+	    var params = {'offset': offset, 'limit': limit};
+	    var headers = {'Accept': "application/json"};
+
+	    return this._performRequest("/app/mobileapps/suggestions/tracks/categories",
+					headers, params);
+	}
+
+	this.getTracksForCategory = function(category, offset, limit) {
+	    var params = {'offset': offset, 'limit': limit};
+	    var headers = {'Accept': "application/json"};
+
+	    return this._performRequest("/app/mobileapps/suggestions/tracks/categories/" + category,
+					headers, params);
+	}
     };
 
-    function urlencode(parameters) {
-	var arr = [];
-	for (var key in parameters) {
-	    arr.push(key + "=" + parameters[key]);
-	}
-	return arr.join("&");
-    }
 
-    function build_query_url(resource, parameters) {
-	url = constants.base_uri + "/" + resource + ".json" + "?" + urlencode(parameters);
-	return url;
-    }
+    // Create a soundcloud client instance
+    var sc = new SoundcloudClient();
 
-    function parse_tracks(buffer)
-    {
+    plugin.register("/", function(offset, limit) {
 	var result = [];
 
-	try {
-	    tracks = eval(res.body);
+	if (offset != 0)
+	    return result;
 
-	    tracks.forEach(function(track) {
-		var item = {};
-		item.type = plugin.item.TYPE_MUSIC_TRACK;
-		item.metadata = {};
-		item.metadata.title = track.title;
-		item.metadata.description = track.description;
+	// Create categories entry
+	var item = {};
+	item.metadata = {};
+	item.type = plugin.item.TYPE_FOLDER;
+	item.uri = plugin.URI_PREFIX + "/categories";
+	item.metadata.title = "Categories";
+	item.metadata.description = "Browse tracks suggestions by category";
 
-		if (track.artwork_url)
-		    item.metadata.image = track.artwork_url;
-		else if (track.waveform_url)
-		    item.metadata.image = track.waveform_url;
-		else
-		    item.metadata.image = track.user.avatar_url;
+	result.push(item);
 
-		item.uri = track.stream_url + "&consumer_key=" + constants.client_id;
+	return result;
+    });
 
-		result.push(item);
-	    });
-	} catch(e) {
-	    service.log("Failed to parse result: " + e.message);
-	    return [];
+    plugin.register("/categories", function(offset, limit) {
+	var result = [];
+	var cnt = 0;
+	var categories = sc.getCategories(offset, limit);
+
+	categories.music.forEach(function(category) {
+
+	    if (cnt < offset) {
+		cnt++;
+		return;
+	    }
+
+	    if (cnt == offset + limit)
+		return;
+
+	    var item = {};
+	    item.metadata = {};
+
+	    item.type = plugin.item.TYPE_FOLDER;
+	    item.uri = plugin.URI_PREFIX + "/categories/" + category.title;
+	    item.metadata.title = category.title;
+	    result.push(item);
+	});
+
+	return result;
+    });
+
+    function durationToString(duration)
+    {
+	var s = Math.round(duration / 1000);
+	var m = Math.round(s / 60);
+	var h = Math.round(m / 60);
+	m -= h*60;
+	s -= m*60;
+
+	var ss = (s + "0").slice(0, 2);
+	var sm = m;
+
+	var result = "";
+	if (h != 0) {
+	    sm = (m + "0").slice(0, 2);
+	    result += h + ":";
 	}
-
+	result += sm + ":" + ss;
 
 	return result;
     }
 
-    plugin.register("/", function(offset, limit) {
-	var query =
-	    build_query_url("tracks", {
-		"consumer_key": constants.client_id,
-		"filter": "streamable",
-		"offset": offset,
-		"limit": limit,
-		"order": "hotness"
-	    });
+    plugin.register("/categories/*", function(offset, limit, category) {
+	var result = [];
+	var cnt = 0;
+	var tracks = sc.getTracksForCategory(category, offset, limit);
 
-	res = http.get(query);
-	if (res.status != 200) return [];
-	return parse_tracks(res.body);
-    });
+	tracks.collection.forEach(function(track) {
 
-    plugin.search(function(keywords, limit) {
-	var query =
-	    build_query_url("tracks", {
-		"consumer_key": constants.client_id,
-		"filter": "streamable",
-		"offset": 0,
-		"limit": limit,
-		"q": keywords.join("+")
-	    });
+	    if (cnt < offset) {
+		cnt++;
+		return;
+	    }
 
-	res = http.get(query);
-	if (res.status != 200) return [];
-	return parse_tracks(res.body);
+	    if (cnt == offset + limit)
+		return;
+
+	    var item = {};
+	    item.metadata = {};
+	    item.uri = track.stream_url;
+	    item.type = plugin.item.TYPE_MUSIC_TRACK;
+	    item.metadata.title = track.title;
+	    item.metadata.length = durationToString(track.duration);
+	    item.metadata.image = track.artwork_url;
+	    item.metadata.artist = track._embedded.user.username;
+	    item.metadata.likes = track._embedded.stats.likes_count;
+	    item.metadata.plays = track._embedded.stats.playback_count;
+
+	    result.push(item);
+
+	});
+
+	return result;
     });
 
 }) (this);
