5b42c9ef6293179ac9882464ef45bdc1d81d5dd7
Henrik Andersson <henrik.4e@gmail.com>
2019-11-02 08:20:30 +0100

    Add browser and entry components

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,8 +5,16 @@ class Api {
 
 
     getProviders() {
-
 	var url = 'api/v1/providers'
+	return fetch(url, {mode: 'same-origin'})
+	    .then((response) => response.json())
+	    .catch((err) => {
+		console.log('Fetch Error :-S', err);
+	    });
+    }
+
+    getEntries(uri) {
+	var url = 'api/v1/' + uri;
 	return fetch(url, {mode: 'same-origin'})
 	    .then((response) => response.json())
 	    .catch((err) => {
@@ -22,43 +30,40 @@ class ProviderCard extends Inferno.Component {
 
     render(props, state) {
 	return Inferno.h('div', {
+	    class: 'card',
 	}, [
-	    Inferno.h('div', {
-	    }, [
-		Inferno.h('div', {
-		}, [
-		    Inferno.h('img', {
-			src: 'api/v1/cache?resource=' + encodeURIComponent(props.provider.icon)
-		    })
-		]),
 
-		Inferno.h('div', {
-		}, [
-		    Inferno.h('strong', {}, [
-			Inferno.h('a', {href: props.provider.homepage}, props.provider.name),
-			", "
-		    ]),
-		    props.provider.description,
+	    Inferno.h('div', {
+		class: 'section',
+	    }, [
+		Inferno.h('img', {
+		    src: 'api/v1/cache?resource=' + encodeURIComponent(props.provider.icon)
+		})
+	    ]),
+
+	    Inferno.h('div', {
+		class: 'section',
+	    }, [
+		Inferno.h('strong', {}, [
+		    Inferno.h('a', {href: props.provider.homepage}, props.provider.name),
+		    ", "
 		]),
+		props.provider.description,
 	    ]),
 	])
     }
 }
 
-class Application extends Inferno.Component {
+class ProviderCollection extends Inferno.Component {
     constructor(props) {
-	super(props);
-
+	super(props)
 	this.api = new Api('admin', 'password')
-
 	this.state = {
-	    authenticated: false,
 	    providers: [],
 	}
     }
 
     componentDidMount() {
-
 	this.api.getProviders()
 	    .then((providers) => {
 		this.setState({
@@ -70,19 +75,114 @@ class Application extends Inferno.Component {
     }
 
     render(props, state) {
-
         return Inferno.h('div', {
-	},
-			 Inferno.h('div', {},
-				   state.providers.map(provider => {
-				       return Inferno.h('div', {},
-							Inferno.h(ProviderCard, {provider: provider})
-						       )
-				   })
-				  )
-			)
-
+	    class: 'container'
+	}, [
+	    state.providers.map(provider => {
+		return Inferno.h('div', {},
+				 Inferno.h(ProviderCard, {provider: provider})
+				)
+	    })
+	])
     }
+}
+
+class NameValue extends Inferno.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render(props, state) {
+	return Inferno.h('div', {class: 'row'}, [
+	    Inferno.h('b', {}, props.name + ': '),
+	    Inferno.h('span', {}, props.value),
+	])
+    }
+}
+
+class Metadata extends Inferno.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render(props, state) {
+	return Inferno.h('div', {class: 'row'}, [
+	    props.image ? Inferno.h('img', {class: 'logo', src: props.image}) : undefined,
+	    props.artist ? Inferno.h(NameValue, {
+		name: 'Artist', value: props.artist
+	    }) : undefined,
+
+	    props.on_air ? Inferno.h(NameValue, {
+		name: 'On air', value: props.on_air
+	    }) : undefined,
+
+	])
+    }
+}
+
+class EntryItem extends Inferno.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render(props, state) {
+	return Inferno.h('div', {
+	    class: 'entry'
+	}, [
+	    Inferno.h('h3', {}, props.entry.metadata.title),
+	    Inferno.h('p', {}, props.entry.metadata.description),
+	    Inferno.h(Metadata, props.entry.metadata)
+	])
+    }
+}
+
+class Browser extends Inferno.Component {
+    constructor(props) {
+	super(props);
+	this.api = new Api('admin', 'password');
+	
+	this.state = {
+	    entries: []
+	};
+    }
+
+    componentDidMount() {
+	// fetch uri and set state
+	this.api.getEntries(this.props.uri)
+	    .then((entries) => {
+		this.setState({
+		    entries: entries
+		})
+	    }).catch((err) => {
+		console.log('Failed: ' + err)
+	    })
+    }
+
+    render(props, state) {
+	return Inferno.h('div', {
+	    class: 'browser',
+	}, state.entries.map(entry => {
+	    return Inferno.h(EntryItem, {entry: entry})
+	}))
+    }
+}
+
+class Application extends Inferno.Component {
+    constructor(props) {
+	super(props);
+
+	this.state = {
+	    authenticated: false,
+	}
+    }
+
+    render(props, state) {
+	return Inferno.h('div', {}, [
+	    Inferno.h(Browser, {uri: '/providers/rad.io/nearest'}),
+	    Inferno.h(ProviderCollection, {})
+	]);
+    }
+    
 }
 
 Inferno.render(Inferno.h(Application, {}), document.getElementById('app'));
